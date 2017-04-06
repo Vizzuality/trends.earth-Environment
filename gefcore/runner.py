78ccff638668e6f2944bc07c6122b247e7f62444
rrequero <rrequero@gmail.com>
2017-04-06 13:38:15 +0200

    improve gee authentication

--- gefcore/runner.py
+++ gefcore/runner.py
@@ -15,14 +15,16 @@ from gefcore.api import patch
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV = os.getenv('ENV')
+EE_SERVICE_ACOUNT = os.getenv('EE_SERVICE_ACCOUNT', None)
+if EE_SERVICE_ACOUNT:
+    logging.info('Authenticating earth engine')
+    gee_credentials = ServiceAccountCredentials.from_p12_keyfile(
+        os.getenv('EE_SERVICE_ACCOUNT', ''),
+        os.path.join(PROJECT_DIR, 'privatekey.pem'),
+        scopes = ee.oauth.SCOPE
+    )
 
-gee_credentials = ServiceAccountCredentials.from_p12_keyfile(
-    os.getenv('EE_SERVICE_ACCOUNT', ''),
-    os.path.join(PROJECT_DIR, 'privatekey.pem'),
-    scopes = ee.oauth.SCOPE
-)
-
-ee.Initialize(gee_credentials)
+    ee.Initialize(gee_credentials)
 
 def change_status_ticket():
     """Ticket status changer"""
