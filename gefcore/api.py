8c957a577942634c0f70ca27ce0b20fdd5c26b52
rrequero <rrequero@gmail.com>
2017-04-06 13:49:56 +0200

    update login

--- gefcore/api.py
+++ gefcore/api.py
@@ -17,13 +17,13 @@ def login():
 
 def patch_execution(json):
     jwt = login()
-
     response = requests.patch(API_URL + '/api/v1/execution/'+ EXECUTION_ID, json=json, headers={'Authorization': 'Bearer ' + jwt})
     if response.status_code != 200:
         print('Error doing request.')
         print(response)
 
 def save_log(json):
+    jwt = login()
     response = requests.post(API_URL + '/api/v1/execution/'+ EXECUTION_ID + '/log', json=json, headers={'Authorization': 'Bearer ' + jwt})
     if response.status_code != 200:
         print('Error doing request.')
